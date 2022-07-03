f21421b6620628fc6b8b21821341ac8130e5931e
Oliver Hayman <ollie.hayman7@gmail.com>
2022-07-03 21:32:00 +0100

    add additional

--- causalimpact/data.py
+++ causalimpact/data.py
@@ -141,6 +141,7 @@ def process_input_data(
         cimodel.check_input_model(model, pre_data, post_data)
     else:
         model = cimodel.build_default_model(
+            model_components,
             observed_time_series,
             normed_data[0] if model_args['standardize'] else pre_data,
             normed_data[1] if model_args['standardize'] else post_data,
